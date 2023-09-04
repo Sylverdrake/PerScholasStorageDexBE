fc61d38a3d14f20a01303ed6d34a01d05d882401
Sylverdrake <cavitevitch@gmail.com>
2023-09-03 21:23:18 -0400

    added schema

---
+++ models/Items.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const itemSchema = new Schema
+({
+    name: {type: String, required: true},
+    location: {type: String, required: true},
+    category: {type: String, required: true},
+    description: {type: String, required: true},
+},
+//Tells us when property was created and updated.
+{timestamps: true})
+
+module.exports = mongoose.model('Item', itemSchema)
