b5ca4cf7684a81c2dc3e4b38c3223f5fbde9080d
Sylverdrake <cavitevitch@gmail.com>
2023-09-04 00:09:37 -0400

    added GET routes, getSingular route not fully functional yet

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,11 +1,50 @@
 const Item = require('../models/itemModel');
 
 //READ/GET All Items
+const getAllItems = async (req, res) =>
+{
+    //variable which retrieves all items asynchronously, sorts by most recent.
+    const items = await Item.find({}).sort({createdAt: -1})
+    res.status(200).json(items)
+}
 
 //READ/GET Specific Item
+const getItem = async (req, res) =>
+{
+    const { id } = req.params
+    const item = await Item.findById(id)
+        if(!item)
+        {
+            return res.status(404).json({error: 'Item does not exist'})
+        }
+
+    res.status(200).json(item)
+}
 
 //CREATE/POST New Item
+const createItem = async (req, res) =>
+{
+    //create a body with destructured elements
+    const {name, location, category, description} = req.body
 
+    //Try-Catch to add document to database
+    try
+    {
+        const newItem = await Item.create({name, location, category, description});
+        res.status(200).json(newItem)
+    }
+    catch(error)
+    {
+        res.status(400).json({error: error.message})
+    }
+}
 //DELETE Item
 
 //UPDATE Item
+
+module.exports = 
+{
+    getAllItems,
+    getItem,
+    createItem
+}
--- routes/items.js
+++ routes/items.js
@@ -1,36 +1,17 @@
 const express = require('express');
-const Item = require('../models/itemModel')
+const {getAllItems, getItem, createItem} = require('../controllers/itemController');
+
 const router = express.Router();
 
 //READ/GET All Items
-router.get('/', (req, res)=>
-{
-    res.json({msg: 'Read/Get All Items '})
-})
+router.get('/', getAllItems)
 
 //READ/GET Specific Item
-router.get('/:id', (req,res)=>
-{
-    res.json({msg: 'Read/Get Single Item'})
-})
+router.get('/:id', getItem)
 
 //CREATE/POST New Item
-router.post('/', async (req, res) =>
-{
-    //create a body with destructured elements
-    const {name, location, category, description} = req.body
+router.post('/', createItem)
 
-    //Try-Catch
-    try
-    {
-        const newItem = await Item.create({name, location, category, description});
-        res.status(200).json(newItem)
-    }
-    catch(error)
-    {
-        res.status(400).json({error: error.message})
-    }
-})
 
 //DELETE Item
 router.delete('/:id', (req, res) =>
